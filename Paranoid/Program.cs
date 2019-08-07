1d2c6a30ad20a4ba464a88de5087c35181e6c3d8
karinamalgazhdarova <malgazhdarova.karina@gmail.com>
2019-08-07 23:56:21 +0600

    Добавьте файлы проекта.

---
+++ Paranoid/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paranoid
+{
+   
+
+    class Program
+    {
+       
+
+       
+        static void Main(string[] args)
+        {
+            Task task = new Task();
+            task.TaskName();
+            task.StartDateTask();
+            task.EndDateTask();
+            task.DateEstimateTask();
+            task.DescriptionTask();
+
+            
+
+          
+
+            //Console.WriteLine("                                 ");
+            //Console.WriteLine("----------------------------------- ");
+
+            Console.WriteLine("Имя задачи: " + task.getTaskName());
+            Console.WriteLine("Дата начала задачи: " + task.getStartDateTask());
+            Console.WriteLine("Дата окончания задачи: " + task.getEndDateTask());
+            Console.WriteLine("Оценка по времени: " + task.getDateEstimateTask());
+            Console.WriteLine("Описание: " + task.getDescriptionTask());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Paranoid/Task.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Paranoid
+{
+    class Task
+    {
+        string taskName;
+        DateTime startDateTask;
+        string userInput;
+        DateTime endDateTask;
+        int dateEstimate;
+        string description;
+        
+        public void TaskName()
+        {
+            try
+            {
+                Console.WriteLine("Введите имя задачи:");
+                taskName = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(taskName))
+                {
+                    throw new Exception("Значение 'Имя задачи' не введено");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                TaskName();
+
+            }
+        }
+        public String getTaskName() {
+            return taskName;
+        }
+
+        public void StartDateTask()
+        {
+            try
+            {
+                Console.WriteLine("Введите дату начала задачи в формате 00.00.0000:");
+                userInput = Console.ReadLine();
+               
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    throw new Exception("Значение 'Дата начала задачи' не введено");
+
+                }
+
+                startDateTask = DateTime.Parse(userInput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.ToString()}");
+                Console.WriteLine($"{ex.Message}");
+                StartDateTask();
+
+            }
+           
+        }
+        public DateTime getStartDateTask() {
+            return startDateTask;
+        }
+
+        public void EndDateTask()
+        {
+            try
+            {
+                Console.WriteLine("Введите дату окончания задачи в формате 00.00.0000:");
+                userInput = Console.ReadLine();
+                
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    throw new Exception("Значение 'Дата окончания задачи' не введено");
+
+                }
+                endDateTask = DateTime.Parse(userInput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                EndDateTask();
+
+            }
+        }
+        public DateTime getEndDateTask()
+        {
+            return endDateTask;
+        }
+
+        public void DateEstimateTask()
+        {
+            try
+            {
+                Console.WriteLine("Введите оценку по времени: ");
+                userInput = Console.ReadLine();
+              
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    throw new Exception("Значение 'Оценка по времени' не введено ");
+
+                }
+
+                int i;
+               
+                if (int.TryParse(userInput, out i) == false)
+                {
+                    throw new Exception("Значение не соответствует числу");
+                }
+                dateEstimate = Int16.Parse(userInput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                DateEstimateTask();
+
+            }
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                throw new Exception("Значение 'Оценка по времени' не введено ");
+
+            }
+           
+}
+        public int getDateEstimateTask()
+        {
+            return dateEstimate;
+        }
+
+       
+
+        public void DescriptionTask()
+        {
+            try
+            {
+                Console.WriteLine("Введите описание: ");
+                description = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(description))
+                {
+                    throw new Exception("Значение 'Описание' не введено ");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                DateEstimateTask();
+
+            }
+        }
+        public String getDescriptionTask()
+        {
+            return description;
+        }
+
+
+    }
+}
